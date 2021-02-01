82bd244db301671a76a81a14524eab61767e3d52
Iris <iris.souza@zup.com.br>
2021-02-01 15:40:37 -0300

    Adding Animal's dictionary for each Zoo

---
+++ src/com/br/zup/exercicioZoologico.java
@@ -0,0 +1,14 @@
+package com.br.zup;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class exercicioZoologico {
+    public static void main(String[] args) {
+
+        Map<String, List<String>> dicionarioAnimais = new HashMap<>();
+
+    }
+
+}
